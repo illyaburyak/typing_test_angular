18b6abf2f91b744b7b01d6885e92c84efc4179c6
Illya Buryak <illyabenavente@gmail.com>
2021-06-23 14:21:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {lorem} from 'faker';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'optionalProj';
+  randomText = lorem.sentence();
+  textInput = '';
+  winningNumber = 0;
+
+  detectTextInput(value: string) {
+    this.textInput = value;
+  }
+
+  compare(randomLetter: string, inputLetter: string) {
+    if (!inputLetter) {
+      return 'pending';
+    }
+    return randomLetter === inputLetter ? 'correct' : 'incorrect';
+  }
+
 }
+
+
